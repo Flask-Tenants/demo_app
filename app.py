207e22975cb4015a77dfe9b16fd5e3c9046b5ad4
Cory Cline <cory@corycline.com>
2024-07-05 19:04:52 -0500

    Clarified comments

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def create_app():
     # Register error handlers
     register_error_handlers(app)
 
-    # Initialize tenants app
+    # Initialize Flask-Tenants
     flask_tenants = FlaskTenants(app, tenant_model=Tenant, domain_model=Domain, db=db, tenant_url_prefix='/_tenant')
     flask_tenants.init()
 
